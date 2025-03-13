85fac0fbce8762dc50ef58b547c0df4243263227
hozumi <falrndTwitter@gmail.com>
2025-03-14 00:19:03 +0900

    ranking refinement

--- src/api/model/ranking.rs
+++ src/api/model/ranking.rs
@@ -9,15 +9,15 @@ pub static EPOCH: NaiveDate = NaiveDate::from_ymd_opt(2017, 5, 7).unwrap();
 
 /// 月間全部門トップ3
 #[derive(Debug, Clone, Deserialize)]
-pub struct AllMonthly(pub HashMap<Category, Vec<Info>>);
+pub struct AllMonthly(pub HashMap<Category, Vec<RankerMonthly>>);
 
 /// 月間部門別トップ1000
 #[derive(Debug, Clone, Deserialize)]
-pub struct SectionMonthly(pub Vec<Info>);
+pub struct SectionMonthly(pub Vec<RankerMonthly>);
 
 /// デイリートップ1000
 #[derive(Debug, Clone, Deserialize)]
-pub struct Daily(pub Vec<DailyInfo>);
+pub struct Daily(pub Vec<RankerDaily>);
 
 /// 部門名
 // todo: リストを提供する
@@ -25,7 +25,7 @@ pub struct Daily(pub Vec<DailyInfo>);
 pub struct Category(pub String);
 
 #[derive(Debug, Clone, Deserialize)]
-pub struct Info {
+pub struct RankerMonthly {
     /// デイリーランキングトップ10入賞回数 (1-10位)
     pub top_10: Vec<u32>,
     /// トップ1獲得回数
@@ -34,26 +34,26 @@ pub struct Info {
 
     /// メダル数
     pub sort: u32,
-    /// オーナー番号
-    pub user_id: shop::UserId,
-    /// ショップ番号
-    pub shop_id: shop::Id,
-    /// ショップ名
-    pub shop_name: String,
-    /// 街ID
-    pub area_id: area::Id,
-    /// キャッチコピー
-    pub comment: String,
+
+    #[serde(flatten)]
+    pub data: RankerCommonData,
 }
 
 #[derive(Debug, Clone, Deserialize)]
-pub struct DailyInfo {
+pub struct RankerDaily {
     /// 座標
     #[serde(flatten)]
     pub pos: Position,
 
     /// ポイントもしくは売上高メダル数
     pub point: u32,
+
+    #[serde(flatten)]
+    pub data: RankerCommonData,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct RankerCommonData {
     /// オーナー番号
     pub user_id: shop::UserId,
     /// ショップ番号
